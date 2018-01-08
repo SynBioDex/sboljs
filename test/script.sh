be4e3c78fc2a60af828990ff854f73e5e0366d68
Zach Zundel <me@zachzundel.com>
2018-01-08 16:19:58 -0700

    Continue through failed tests.

--- test/script.sh
+++ test/script.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+ERROR=0
+
 for f in SBOLTestSuite/SBOL2/*
 do
   echo "Testing "$f
-  validation_result=$(node ReadWriteTestlibSBOL.js $PWD/$f);
-  if [[ $validation_result != "Validation successful, no errors." ]]; then
-      echo $validation_result;
-      exit 1;
-  else echo "   Successful!"; fi
+  validation_result=$(node ReadWriteTestlibSBOL.js $PWD/$f)
+  if [[ $validation_result == "" ]]
+  then
+      echo "   Successful!"
+  else
+      echo "    Failed."
+      echo "$validation_result"
+      ERROR=1
+  fi
 done
+
+exit $ERROR
